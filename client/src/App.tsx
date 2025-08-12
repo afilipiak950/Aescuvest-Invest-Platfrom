1fa73b43f07ea20e91dba7604d46343e3e65be5d
intellywave <41669181-intellywave@users.noreply.replit.com>
2025-08-12 12:20:29 +0000

    Restored to 'c8cee01123f673bba0cc943a6d78dc3f2d9347fe'
    Replit-Restored-To: c8cee01123f673bba0cc943a6d78dc3f2d9347fe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -76,7 +76,7 @@ function AppContent() {
           <AuthenticatedLayout>
             <Switch>
               <Route path="/">
-                <DueDiligence />
+                <Dashboard />
               </Route>
               <Route path="/all-deals">
                 <AllDealsPage />
